fe15ede5614087298c0d7f340eefc3345afdf655
Fedeeherrera <fedeeherrera2001@gmail.com>
2024-11-29 11:55:29 -0300

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import {type User} from './types'
 import './App.css'
+import UsersList from './components/UsersList'
 
 function App() {
+  const [users, setUsers] = useState<User[]>([])
   const URL_API = `https://randomuser.me/api?results=100`
   useEffect(() => {
     fetch(URL_API)
-    .then(response => response.json())
-    .then(data => console.log(data))
-  })
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.results)
+        console.log(data.results)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }, [])
   return (
-    <div className='app'>
+    <div className="app">
       <h1>Prueba tecnica</h1>
+      <UsersList users={users}/>
     </div>
   )
 }
